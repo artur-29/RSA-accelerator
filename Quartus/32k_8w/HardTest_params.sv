21f57a0aeaf472ceb883d87c8016631dde1fec2c
artur-29 <100810476+artur-29@users.noreply.github.com>
2022-03-03 13:04:50 +0000

    Add files via upload

---
+++ Quartus/32k_8w/HardTest_params.sv
@@ -0,0 +1,9 @@
+package HardTest_params;
+    
+    localparam   RSA_DATA_WIDTH        =     32; // k
+    localparam   RSA_WORD_SIZE         =     8;
+    localparam   UART_WORD_SIZE        =     8;
+    localparam   UART_WORDS            =     RSA_WORD_SIZE/UART_WORD_SIZE;
+    localparam   RSA_DEVICE_FAMILY     =     "Cyclone V";
+
+endpackage
